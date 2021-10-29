867adfcb1ddc85ebb1376a13d0335ad7fd60855a
muzsay22 <77saymuz@stud.akademiet.no>
2021-10-29 13:45:29 +0200

    .

--- app/src/main/java/com/example/horoskop_app/horoskopActivity.kt
+++ app/src/main/java/com/example/horoskop_app/horoskopActivity.kt
@@ -51,7 +51,7 @@ class horoskopActivity : AppCompatActivity() {
             },
             { error ->
                 //Håndterer eventuelle feil
-                view.text = "Kunne ikke laste horoskop!"
+                view.text = "Kunne ikke laste horoskop!!"
             }
         )
 
